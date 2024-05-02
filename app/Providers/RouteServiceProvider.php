8d3f233211f89661a149888f961d9b2f81561e12
Moch Farrel <91719525+mochfarrel06@users.noreply.github.com>
2024-05-02 13:34:06 +0700

    Feat/auth: Multi Auth Setup with Breeze (#1)
    * feat(auth): Setting up Laravel Breeze Auth in Project
    
    * feat(auth): Add new Columns in Users Table
    
    * feat(auth): Add New Seeders for User and Admin
    
    * feat(auth): Create Dashboards for Users
    
    * feat(auth): Create Middleware for Multi Auth
    
    * feat(auth): Redirect to Dashboard Depending on ROle
    
    * feat(auth): Create Separate Route Files for Admin

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/dashboard';
+    public const ADMIN = 'admin/dashboard';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
@@ -35,6 +36,9 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware(['web', 'auth', 'role:admin'])
+                ->group(base_path('routes/admin.php'));
         });
     }
 }
---
+++ routes/admin.php
@@ -0,0 +1,10 @@
+<?php
+
+// Admin
+
+use App\Http\Controllers\Admin\AdminDashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::get('dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
+});
