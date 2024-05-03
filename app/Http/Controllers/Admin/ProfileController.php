dbd0d556d78197ed577f8b823260c05af896dbaf
MochFarrel06 <mochfarrel2606@gmail.com>
2024-05-03 14:27:08 +0700

    feat(profile): Working on admin profile, edit data for database

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\ProfileUpdateRequest;
+use Auth;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -17,6 +18,13 @@ class ProfileController extends Controller
 
     function updateProfile(ProfileUpdateRequest $request): RedirectResponse
     {
+        $user = Auth::user();
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+
+        toastr('Update successfully!!!', 'success');
         return redirect()->back();
     }
 }
